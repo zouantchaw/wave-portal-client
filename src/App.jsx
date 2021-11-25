7876138f92046fc13a378a7053347ca1e340671b
Wielfried Zouantcha <44246692+zouantchaw@users.noreply.github.com>
2021-11-25 19:54:03 +0000

    event listener that monitors new wave creation

--- src/App.jsx
+++ src/App.jsx
@@ -144,8 +144,37 @@ export default function App() {
     setWaveInput(event.target.value)
   }
 
+  // Listen to emitter events
   useEffect(() => {
     checkIfWalletIsConnected();
+
+    let wavePortalContract;
+
+    const onNewWave = (from, timestamp, message) => {
+      console.log("NewWave", from, timestamp, message);
+      setAllWaves(prevState => [
+        ...prevState,
+        {
+          address: from,
+          timestamp: new Date(timestamp * 1000),
+          message: message,
+        },
+      ]);
+    };
+
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+      wavePortalContract.on('NewWave', onNewWave);
+    }
+
+    return () => {
+      if (wavePortalContract) {
+        wavePortalContract.off('NewWave', onNewWave);
+      }
+    };
   }, []);
 
   return (
